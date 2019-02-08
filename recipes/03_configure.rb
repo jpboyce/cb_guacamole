67fc188ceb730f28486782553a8e954cb7d6d419
Jesse Boyce <jesse@jpboyce.org>
2019-02-09 09:00:32 +1000

    guacamole.properties template work

---
+++ recipes/03_configure.rb
@@ -0,0 +1,40 @@
+#
+# Cookbook:: cb_guacamole
+# Recipe:: 03_config
+#
+# The MIT License (MIT)
+#
+# Copyright:: 2019, Jesse Boyce
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# Create guacamole.properties file off template and populate
+template '/etc/guacamole/guacamole.properties' do
+  source 'guacamole.properties.erb'
+  mode '0755'
+  variables(guacd_hostname: node['cb_guacamole']['guacd']['hostname'],
+    guacd_port: node['cb_guacamole']['guacd']['port'])
+  action :create
+end
+
+# Placeholder for logback.xml file
+
+# Placeholder for user auth
+
+# Placeholder for guacd SSL
