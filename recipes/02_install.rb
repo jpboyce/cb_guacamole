8da9066f13ad09825f4343fbe786f0825c50e1d4
Jesse Boyce <jesse@jpboyce.org>
2019-02-09 09:00:15 +1000

    1st Working pass for install

---
+++ recipes/02_install.rb
@@ -0,0 +1,45 @@
+#
+# Cookbook:: cb_guacamole
+# Recipe:: 02_install
+#
+# The MIT License (MIT)
+#
+# Copyright:: 2019, Jesse Boyce
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# Install guacamole server
+bash 'install_guacamole_server' do
+  cwd '/etc/guacamole'
+  code <<-EOH
+  ./configure --with-init-dir=/etc/init.d
+  make
+  make install
+  make
+  touch install_completed
+  ldconfig
+  EOH
+  action :run
+  not_if {::File.exists?('/etc/guacamole/install_completed')}
+end
+
+# Install tomcat server
+yum_package 'tomcat' do
+  action :install
+end
