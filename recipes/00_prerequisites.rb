3030637ec6f657f41202bdfda2769697e8f4acfc
Jesse Boyce <jesse@jpboyce.org>
2019-02-09 08:59:37 +1000

    Working 1st pass

--- recipes/00_prerequisites.rb
+++ recipes/00_prerequisites.rb
@@ -1,5 +1,48 @@
 #
-# Cookbook:: guacamole
+# Cookbook:: cb_guacamole
 # Recipe:: 00_prerequisites
 #
-# Copyright:: 2019, The Authors, All Rights Reserved.
+# The MIT License (MIT)
+#
+# Copyright:: 2019, Jesse Boyce
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# Install EPEL
+yum_package node['cb_guacamole']['packages']['epel'] do
+  action :install
+end
+
+# Do update after EPEL install
+# Don't run if the guacamole server tar file exists because it being there means we've already progressed past needing to do an update or this is a rerun
+execute 'yum_update' do
+  command 'yum -y update'
+  action :run
+  not_if { ::File.exists?("/tmp/guacamole-server-#{node['cb_guacamole']['server']['version']}.tar.gz")}
+end
+
+# Install GCC
+yum_package node['cb_guacamole']['packages']['gcc'] do
+  action :install
+end
+
+# Install Required packages
+yum_package node['cb_guacamole']['packages']['required'] do
+  action :install
+end
